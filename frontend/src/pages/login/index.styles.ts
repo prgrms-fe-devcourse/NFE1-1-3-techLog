2ca9d882979902dadee8f7023b6a992d74c1657f
miin-hyukkk <ham9893@naver.com>
2024-10-23 15:29:05 +0900

    feat: 로그인페이지 UI 구현

---
+++ frontend/src/pages/login/index.styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100%;
+  gap: 2rem;
+  padding: 6rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  /* border: 10px solid blue; */
+  h2 {
+    padding-bottom: 7rem;
+  }
+  div {
+    padding-bottom: 1rem;
+  }
+`;
+export default Container;
