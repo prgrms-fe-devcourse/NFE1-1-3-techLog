57c4042de3894924f2b56b85fa589e9a88e87c9a
chj <bluedog129@gmail.com>
2024-10-24 15:43:54 +0900

    fix: delete updatedAt

--- BackEnd/Models/user.js
+++ BackEnd/Models/user.js
@@ -4,8 +4,7 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
+  createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('User', userSchema);
