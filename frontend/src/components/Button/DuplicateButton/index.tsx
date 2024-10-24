d03813ab68273a9c207930eacdac1f9c054d1c48
이주환 <wnghks145@gmail.com>
2024-10-24 15:19:19 +0900

    design: 중복확인 버튼 색상 변경

--- frontend/src/components/Button/DuplicateButton/index.tsx
+++ frontend/src/components/Button/DuplicateButton/index.tsx
@@ -11,16 +11,16 @@ interface DuplicateButtonProps {
 const Button = styled.button<{ width?: string }>`
   width: ${({ width }) => width || '100%'};
   padding: 1rem 0; /* 높이를 줄이기 위해 패딩 값 조정 */
-  background-color: ${({ disabled }) => (disabled ? '#E6E6E6' : '#000000')};
-  color: ${({ disabled }) => (disabled ? '#9B9B9B' : '#FFFFFF')};
-  font-size: 1.6rem;
+  background-color: ${({ disabled }) => (disabled ? '#000000' : '#000000')};
+  color: ${({ disabled }) => (disabled ? '#EEEEEE' : '#FFFFFF')};
+  font-size: 1.2rem;
   border: none;
   border-radius: 15px;
   cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
-  transition: background-color 0.3s ease;
+  transition: background-color 0.2s ease;
 
   &:hover {
-    background-color: ${({ disabled }) => (disabled ? '#E6E6E6' : '#333333')};
+    background-color: ${({ disabled }) => (disabled ? '#000000' : '#333333')};
   }
 
   &:active {
