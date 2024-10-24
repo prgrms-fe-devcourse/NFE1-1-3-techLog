3a82b4e71a5dd126c1e386b679fd55ebd5b22910
miin-hyukkk <ham9893@naver.com>
2024-10-24 12:41:24 +0900

    feat: 메인UI 틀 구현

--- frontend/src/pages/main/index.styles.ts
+++ frontend/src/pages/main/index.styles.ts
@@ -3,9 +3,13 @@ import styled from 'styled-components';
 // eslint-disable-next-line import/prefer-default-export
 export const Container = styled.div`
   width: 100%;
+  height: 100vh;
   gap: 2rem;
-  padding: 4rem;
+  padding: 6.7rem;
   display: flex;
-  flex-direction: column;
-  align-items: center;
+  background-color: gray;
+  h1 {
+    font-size: 4.5rem;
+    font-weight: 600;
+  }
 `;
