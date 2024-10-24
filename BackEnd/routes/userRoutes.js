e5f6f7eea44ba1348afbae62cbf9c22cda7a30f1
chj <bluedog129@gmail.com>
2024-10-24 14:37:56 +0900

    Feat: routes implementation

---
+++ BackEnd/routes/userRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const postController = require('../controllers/userController');
+
+// user sign up
+
+// user sign in
+
+module.exports = router;
