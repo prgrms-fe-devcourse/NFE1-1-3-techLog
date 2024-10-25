b66cc1e7b6831098785c2a05554ed9c74bb4974a
chj <bluedog129@gmail.com>
2024-10-25 09:29:48 +0900

    Fix: add models

---
+++ BackEnd/models/post.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+  question: { type: String, required: true },
+  simpleAnswer: { type: String },
+  detailAnswer: { type: String },
+  author: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+ 
+module.exports = mongoose.model('Post', postSchema);
