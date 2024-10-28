27b188273d9f12582484cad294a1d1317f00b722
asdfghjk1052@naver.com <asdfghjk1052@naver.com>
2024-10-28 15:26:59 +0900

    fix: response 데이터 구조 수정

--- BackEnd/controllers/userController.js
+++ BackEnd/controllers/userController.js
@@ -8,42 +8,62 @@ const validators = {
   validateUsername: username => {
     if (!username) {
       return {
+        status: 400,
         isValid: false,
-        message: 'id는 필수 입력 사항입니다.',
-        field: 'username',
+        errors: {
+          message: 'id는 필수 입력 사항입니다.',
+          field: 'username',
+        },
       };
     }
-    const usernameRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{6,18}$/;
+    const usernameRegex = /^(?=.*[a-z])(?=.*\d)[A-Za-z\d]{6,18}$/;
     if (!usernameRegex.test(username)) {
       return {
+        status: 400,
         isValid: false,
-        message: 'id는 6~18자의 영문 대/소문자, 숫자를 포함해야 합니다.',
-        field: 'username',
+        errors: {
+          message: 'id는 6~18자의 영문 소문자, 숫자를 포함해야 합니다.',
+          field: 'username',
+        },
       };
     }
-    return { isValid: true };
+    return {
+      status: 200,
+      isValid: true,
+      message: 'id의 조건을 모두 만족합니다.',
+    };
   },
 
   // 비밀번호 유효성 검사 객체
   validatePassword: password => {
     if (!password) {
       return {
+        status: 400,
         isValid: false,
-        message: 'password는 필수 입력 사항입니다.',
-        field: 'password',
+        errors: {
+          message: 'password는 필수 입력 사항입니다.',
+          field: 'password',
+        },
       };
     }
     const passwordRegex =
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{10,18}$/;
     if (!passwordRegex.test(password)) {
       return {
+        status: 400,
         isValid: false,
-        message:
-          '비밀번호는 10~18자의 영문 대/소문자, 숫자, 특수문자(!@#$%^&*)를 포함해야 합니다.',
-        field: 'password',
+        errors: {
+          message:
+            '비밀번호는 10~18자의 영문 대/소문자, 숫자, 특수문자(!@#$%^&*)를 포함해야 합니다.',
+          field: 'password',
+        },
       };
     }
-    return { isValid: true };
+    return {
+      status: 200,
+      isValid: true,
+      message: 'password의 조건을 모두 만족합니다.',
+    };
   },
 };
 
@@ -66,8 +86,12 @@ exports.signupUser = async (req, res) => {
     const existingUser = await User.findOne({ username });
     if (existingUser) {
       return res.status(409).json({
-        message: '이미 등록된 회원입니다.',
-        field: 'username',
+        status: 409,
+        success: false,
+        errors: {
+          message: '이미 등록된 회원입니다.',
+          field: 'username',
+        },
       });
     }
 
@@ -80,13 +104,21 @@ exports.signupUser = async (req, res) => {
 
     // 회원가입 성공할 경우 사용자 데이터 반환
     res.status(201).json({
+      status: 201,
+      success: true,
       message: '회원가입이 성공적으로 완료되었습니다.',
-      user: userDoc,
+      data: {
+        user: userDoc,
+      },
     });
   } catch (e) {
     res.status(500).json({
-      message: '서버 에러 발생',
-      error: e,
+      status: 500,
+      success: false,
+      errors: {
+        message: '서버 에러 발생',
+        error: e,
+      },
     });
   }
 };
@@ -104,18 +136,29 @@ exports.idCheckUser = async (req, res) => {
     const existingUser = await User.findOne({ username });
     if (existingUser) {
       return res.status(409).json({
-        message: '사용중인 id입니다.',
-        field: 'username',
+        status: 409,
+        success: false,
+        errors: {
+          message: '사용중인 id입니다.',
+          field: 'username',
+        },
       });
     } else {
       return res.status(200).json({
+        status: 200,
+        success: true,
         message: '사용 가능한 id입니다.',
+        username: username,
       });
     }
   } catch (e) {
     res.status(500).json({
-      message: '서버 에러 발생',
-      error: e.message,
+      status: 500,
+      success: false,
+      errors: {
+        message: '서버 에러 발생',
+        error: e.message,
+      },
     });
   }
 };
