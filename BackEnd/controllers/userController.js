0d06c48538ea1b790e7e17a33424f66af19a0f7b
asdfghjk1052@naver.com <asdfghjk1052@naver.com>
2024-10-28 11:37:22 +0900

    feature: 회원가입 유효성 검사 로직 구현

--- BackEnd/controllers/userController.js
+++ BackEnd/controllers/userController.js
@@ -2,22 +2,65 @@ const path = require('path');
 const User = require(path.join(__dirname, '../models/user'));
 const bcryptjs = require('bcryptjs');
 
-// -------------------비밀번호 해시 생성 함수-------------------
-const hashPassword = password => {
-  const saltRounds = 10; // 해시 생성 시 사용되는 salt
-  return bcryptjs.hashSync(password, saltRounds);
+// -------------------유효성 검사 객체---------------------------
+const validators = {
+  // 아이디 유효성 검사 객체
+  validateUsername: username => {
+    if (!username) {
+      return {
+        isValid: false,
+        message: 'id는 필수 입력 사항입니다.',
+        field: 'username',
+      };
+    }
+    const usernameRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{6,18}$/;
+    if (!usernameRegex.test(username)) {
+      return {
+        isValid: false,
+        message: 'id는 6~18자의 영문 대/소문자, 숫자를 포함해야 합니다.',
+        field: 'username',
+      };
+    }
+    return { isValid: true };
+  },
+
+  // 비밀번호 유효성 검사 객체
+  validatePassword: password => {
+    if (!password) {
+      return {
+        isValid: false,
+        message: 'password는 필수 입력 사항입니다.',
+        field: 'password',
+      };
+    }
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{10,18}$/;
+    if (!passwordRegex.test(password)) {
+      return {
+        isValid: false,
+        message:
+          '비밀번호는 10~18자의 영문 대/소문자, 숫자, 특수문자(!@#$%^&*)를 포함해야 합니다.',
+        field: 'password',
+      };
+    }
+    return { isValid: true };
+  },
 };
 
 // -------------------회원가입 로직---------------------------
 exports.signupUser = async (req, res) => {
   const { username, password } = req.body;
 
-  // 필수 필드 체크
-  if (!username || !password) {
-    return res
-      .status(400)
-      .json({ message: 'id와 password는 필수 입력 사항 입니다.' });
+  const usernameValidation = validators.validateUsername(username);
+  if (!usernameValidation.isValid) {
+    return res.status(400).json(usernameValidation);
   }
+
+  const passwordValidation = validators.validatePassword(password);
+  if (!passwordValidation.isValid) {
+    return res.status(400).json(passwordValidation);
+  }
+
   try {
     // 등록된 회원인지 확인
     const existingUser = await User.findOne({ username });
@@ -29,9 +72,9 @@ exports.signupUser = async (req, res) => {
     }
 
     // 비밀번호 암호화
-    const hashedPassword = hashPassword(password);
+    const hashedPassword = await bcryptjs.hash(password, 10);
     const userDoc = await User.create({
-      username,
+      username: username.trim(),
       password: hashedPassword,
     });
 
@@ -52,11 +95,9 @@ exports.signupUser = async (req, res) => {
 exports.idCheckUser = async (req, res) => {
   const { username } = req.body;
 
-  if (!username || typeof username !== 'string') {
-    return res.status(400).json({
-      message: 'id는 필수 입력 사항입니다.',
-      field: 'username',
-    });
+  const usernameValidation = validators.validateUsername(username);
+  if (!usernameValidation.isValid) {
+    return res.status(400).json(usernameValidation);
   }
 
   try {
