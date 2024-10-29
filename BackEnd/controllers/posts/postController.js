6ed9e3eef16f8c9688ee3ca49328d7b14a99b10e
kimjuyoung99 <jjassb404@naver.com>
2024-10-29 15:56:39 +0900

    [BE] fix : token -> cookie로 변경, 통신 확인

--- BackEnd/controllers/posts/postController.js
+++ BackEnd/controllers/posts/postController.js
@@ -1,13 +1,21 @@
 const path = require('path');
 const Post = require(path.join(__dirname, '../../models/post'));
 
-// Create Post
 exports.createPost = async (req, res) => {
   try {
+    // cookies에서 토큰 확인
+    if (!req.cookies.token) {
+      return res.status(401).json({ 
+        status: 401,
+        success: false,
+        message: "토큰이 존재하지 않습니다."
+      });
+    }
+
     // 토큰 정보 추가
     const post = new Post({
       ...req.body,
-      authorId: req.token, // 테스트용으로 토큰값을 authorId로 사용
+      authorId: req.cookies.token  // req.cookie가 아닌 req.cookies 사용
     });
 
     const savedPost = await post.save();
