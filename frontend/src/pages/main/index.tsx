908bc2f56e50f5dfc3c30bc6af2cb778f9b21c93
miin-hyukkk <ham9893@naver.com>
2024-10-29 17:14:30 +0900

    feat: 훅 분리 및 모달 사용 예시 구현

--- frontend/src/pages/main/index.tsx
+++ frontend/src/pages/main/index.tsx
@@ -1,18 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
 import * as S from './index.styles';
-import useStore from '../../store';
 import Dialog from '../../components/Dialog';
+import ModalRead from '../../components/Modal/ModalRead';
+import ModalForm from '../../components/Modal/ModalForm';
+import useModal from '../../hooks/useModal';
+import useDialog from '../../hooks/useDialog';
 
 export default function Main() {
-  const { activeIndex } = useStore();
-  const Tabs = ['All', 'React', 'CS', 'Network'];
-  const [isDialogOpen, setIsDialogOpen] = useState(true);
-  const handleConfirm = () => {
-    setIsDialogOpen(false);
-  };
-  const handleCancel = () => {
-    setIsDialogOpen(false);
-  };
+  const { isDialogOpen, handleConfirm, handleCancel, Tabs, activeIndex } =
+    useDialog();
+  const {
+    isRegisterModalOpen,
+    handleRegisterSubmit,
+    closeRegisterModal,
+    isReadModalOpen,
+    detailData,
+    isEditModalOpen,
+    closeReadModal,
+    handleDelete,
+    handleEdit,
+    handleEditSubmit,
+    closeEditModal,
+  } = useModal();
+
   return (
     <S.Container>
       {isDialogOpen && (
@@ -25,7 +35,37 @@ export default function Main() {
           cancelTitle="돌아가기"
         />
       )}
+      {isRegisterModalOpen && (
+        <ModalForm
+          initialCategory="React"
+          onSubmit={handleRegisterSubmit}
+          onClose={closeRegisterModal}
+        />
+      )}
+      {isReadModalOpen && (
+        <ModalRead
+          type="read"
+          initialCategory={detailData.category}
+          question={detailData.question}
+          shortAnswer={detailData.shortAnswer}
+          detailedAnswer={detailData.detailedAnswer}
+          onClose={closeReadModal}
+          onDelete={handleDelete}
+          onEdit={handleEdit}
+        />
+      )}
+      {isEditModalOpen && (
+        <ModalForm
+          onSubmit={handleEditSubmit}
+          onClose={closeEditModal}
+          initialCategory={detailData.category}
+          initialQuestion={detailData.question}
+          initialShortAnswer={detailData.shortAnswer}
+          initialDetailedAnswer={detailData.detailedAnswer}
+        />
+      )}
       <h1>{Tabs[activeIndex]}</h1>
+      {/* 여기 아래에 3*3형태로 아이템 박스  */}
     </S.Container>
   );
 }
