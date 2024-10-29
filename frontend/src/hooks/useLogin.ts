98c2e44cc471028314134d4e05a9ff80adffbc77
miin-hyukkk <ham9893@naver.com>
2024-10-29 17:08:47 +0900

    chore: prettier 오류 수정

--- frontend/src/hooks/useLogin.ts
+++ frontend/src/hooks/useLogin.ts
@@ -1,3 +1,17 @@
 export default function useLogin() {
-  return {};
+  const inputs = [
+    {
+      label: '아이디',
+      placeholder: '아이디를 입력하세요',
+      onChange: () => console.log('아이디'),
+      width: '50rem',
+    },
+    {
+      label: '비밀번호',
+      placeholder: '비밀번호를 입력하세요',
+      onChange: () => console.log('비번'),
+      width: '50rem',
+    },
+  ];
+  return { inputs };
 }
