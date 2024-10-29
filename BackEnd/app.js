c8828001e6e22135e4107c7895fc733a8239f582
asdfghjk1052@naver.com <asdfghjk1052@naver.com>
2024-10-29 10:36:12 +0900

    feature: 로그인 구현

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -2,12 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-const cors = require('cors');  // CORS 추가
+const cors = require('cors'); // CORS 추가
 const postRoutes = require(path.join(__dirname, './routes/postRoutes'));
 const userRoutes = require(path.join(__dirname, './routes/userRoutes'));
 
 const app = express();
-
 
 // MongoDB 연결
 mongoose
@@ -20,10 +19,9 @@ mongoose
   });
 
 // 미들웨어 설정
-app.use(cors());  // CORS 미들웨어 추가
+app.use(cors()); // CORS 미들웨어 추가
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 
 // 라우트 설정
 app.use('/posts', postRoutes);
