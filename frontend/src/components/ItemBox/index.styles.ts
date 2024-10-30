4fa0a77afcde203d6f2910de8fb583756bc9af92
이주환 <wnghks145@gmail.com>
2024-10-31 08:13:16 +0900

    feat: 아이템박스 UI 구현, 컴포넌트 분리

---
+++ frontend/src/components/ItemBox/index.styles.ts
@@ -0,0 +1,72 @@
+// index.styles.ts
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  gap: 2rem;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    font-size: 4.5rem;
+    font-weight: 600;
+  }
+`;
+
+export const MainPage = styled.div`
+  width: 100%;
+`;
+
+export const ItemBoxGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr); // 3열 그리드로 수정
+  grid-gap: 20px;
+  width: 100%;
+  height: auto;
+`;
+
+export const ItemBox = styled.div`
+  margin: 10px;
+  height: auto;
+
+  // div 높이
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 33%;
+    font-size: 1.5rem;
+    font-weight: 500;
+  }
+
+  div:nth-child(even) {
+    border: 1px solid black;
+    border-radius: 7px;
+    background-color: white;
+    color: black;
+  }
+
+  div:nth-child(odd) {
+    border-radius: 7px;
+    background-color: black;
+    color: white;
+  }
+`;
+
+export const PlusBtn = styled.button`
+  font-size: 6rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 70px;
+  height: 70px;
+  background-color: white;
+  border: 3px solid black;
+  border-radius: 50%;
+  position: absolute; // PlusBtn 절대위치
+  bottom: 27%;
+  right: 3%;
+  cursor: pointer;
+`;
