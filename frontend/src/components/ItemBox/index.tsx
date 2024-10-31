58e9f0d4a3b34ba52a11c129916796adb39a299e
miin-hyukkk <ham9893@naver.com>
2024-10-31 15:47:33 +0900

    feat: 문제 박스 기능 추가 구현
    색깔 구분 및 클릭시 토글 기능 수정

--- frontend/src/components/ItemBox/index.tsx
+++ frontend/src/components/ItemBox/index.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
 import * as S from './index.styles';
+import { QaDataWithOnClick } from '../../interface/qaData';
 
-interface ItemBoxProps {
-  question: string;
-  answer: string;
-  showAnswer: boolean;
-  onClick: () => void;
-}
-
-// 일반 함수 선언으로 컴포넌트를 정의
-const ItemBox: React.FC<ItemBoxProps> = function ({
-  question,
-  answer,
+function ItemBox({
+  shortAnswer,
+  title,
   showAnswer,
   onClick,
-}) {
+  isEven,
+}: Partial<QaDataWithOnClick>) {
   return (
-    <S.ItemBox onClick={onClick}>
-      <div>{showAnswer ? answer : question}</div>
+    <S.ItemBox onClick={onClick} isEven={isEven}>
+      <div>{showAnswer ? shortAnswer : title}</div>
     </S.ItemBox>
   );
-};
+}
 
 export default ItemBox;
