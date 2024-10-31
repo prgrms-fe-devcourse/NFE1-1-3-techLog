202064761401888a0a1c5934c1a52719156527f7
miin-hyukkk <ham9893@naver.com>
2024-10-31 15:46:37 +0900

    feat: 전체조회,문제수정,등록 api 구현

--- frontend/src/api/questionAnswer.ts
+++ frontend/src/api/questionAnswer.ts
@@ -1,4 +1,18 @@
-export const registerQA = async (detailData: QaData) => {
+import API from './config';
+import apiClient from '.';
+import { ModalQaData } from '../interface/modalData';
+
+export const loadQA = async () => {};
+export const deleteQA = async () => {};
+export const registerQA = async (detailData: ModalQaData) => {
   const { data } = await apiClient.post(API.REGISTER, detailData);
+  return data;
+};
+export const editQA = async (detailData: ModalQaData) => {
+  const { data } = await apiClient.post(API.REGISTER, detailData);
+  return data;
+};
+export const loadAllQA = async () => {
+  const { data } = await apiClient.get(API.READ_ALL);
   return data;
 };
