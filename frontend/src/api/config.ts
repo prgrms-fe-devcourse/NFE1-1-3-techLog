fa3b332328d0b7bd9ba5377688081cd2298895c4
miin-hyukkk <ham9893@naver.com>
2024-10-31 11:18:12 +0900

    feat: 질문 관련 api 상수화

--- frontend/src/api/config.ts
+++ frontend/src/api/config.ts
@@ -3,6 +3,11 @@ const BASE_URL = process.env.REACT_APP_BASE_URL;
 const API = {
   BASE_URL: `${BASE_URL}`,
   LOGIN: `user/login`,
+  REGISTER: 'posts/create',
+  DELETE: 'posts/delete',
+  READ: 'posts/read',
+  EDIT: 'posts/update',
+  READ_ALL: 'posts/all',
 };
 
 export default API;
