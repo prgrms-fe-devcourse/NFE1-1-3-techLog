357188e0a582fdd7d74b9f54707fb98f64f5bc29
이주환 <wnghks145@gmail.com>
2024-11-01 10:59:59 +0900

    refactor: config ( 회원가입, 중복확인) 추가

--- frontend/src/api/config.ts
+++ frontend/src/api/config.ts
@@ -3,6 +3,8 @@ const BASE_URL = process.env.REACT_APP_BASE_URL;
 const API = {
   BASE_URL: `${BASE_URL}`,
   LOGIN: `user/login`,
+  SIGNUP: `user/signup`,
+  DUPLICATE: `user/idCheck`,
 };
 
 export default API;
