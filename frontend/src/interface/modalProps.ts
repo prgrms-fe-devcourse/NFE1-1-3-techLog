275091ded8fb082302bb03837d9a3250bd163282
miin-hyukkk <ham9893@naver.com>
2024-11-01 11:51:07 +0900

    feat: 모달프롭스 변경

--- frontend/src/interface/modalProps.ts
+++ frontend/src/interface/modalProps.ts
@@ -1,11 +1,5 @@
-import { Category } from './category';
-
 export interface ModalProps {
   type?: 'register' | 'read' | 'edit';
-  initialCategory: Category;
-  question?: string;
-  shortAnswer?: string;
-  detailedAnswer?: string;
   onSubmit?: (data: any) => void;
   onClose: () => void;
   onDelete?: () => void;
